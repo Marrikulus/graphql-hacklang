0bb7673e39bc56d583a8bd928f47f9d9517e1728
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2019-02-20 23:38:06 +0000

    ran the no-namespace-fallback migration for hacklang

--- src/Language/Source.php
+++ src/Language/Source.php
@@ -51,7 +51,7 @@ class Source
         );
 
         $this->body = $body;
-        $this->length = mb_strlen($body, 'UTF-8');
+        $this->length = \mb_strlen($body, 'UTF-8');
         $this->name = $name !== null ? $name : 'GraphQL';
         $this->locationOffset = $location !== null ? $location : new SourceLocation(1, 1);
 
@@ -74,14 +74,14 @@ class Source
         $line = 1;
         $column = $position + 1;
 
-        $utfChars = json_decode('"\u2028\u2029"');
+        $utfChars = \json_decode('"\u2028\u2029"');
         $lineRegexp = '/\r\n|[\n\r'.$utfChars.']/su';
         $matches = [];
-        preg_match_all($lineRegexp, mb_substr($this->body, 0, $position, 'UTF-8'), &$matches, PREG_OFFSET_CAPTURE);
+        \preg_match_all($lineRegexp, \mb_substr($this->body, 0, $position, 'UTF-8'), &$matches, \PREG_OFFSET_CAPTURE);
 
         foreach ($matches[0] as $index => $match) {
             $line += 1;
-            $column = $position + 1 - ($match[1] + mb_strlen($match[0], 'UTF-8'));
+            $column = $position + 1 - ($match[1] + \mb_strlen($match[0], 'UTF-8'));
         }
 
         return new SourceLocation($line, $column);
